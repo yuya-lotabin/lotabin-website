599037030864b9994c16a10d37b1442692e9ee80
yuya-lotabin <yuya@lotabin.com>
2026-05-29 00:14:25 +0900

    Code error fix

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -6,7 +6,7 @@ export function Hero() {
       {/* VIDEO */}
       <div className="relative h-screen min-h-[900px] w-full">
         <video
-          src="/media/public/media/mk2-home-hero.ver3.mp4"
+          src="/public/media/mk2-home-hero.ver3.mp4"
           autoPlay
           muted
           loop
